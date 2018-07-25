adf4e2519eb14e297317cd81c7c61966a58ac664
gattaigames <gattaigames@gmail.com>
2018-07-25 15:44:05 +0800

    First upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1532504318,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"kfc_logo.jpg",
+		"kfclogo.jpg",
+		"channel.html"
+	]
+}
